19a54205110a8fc3b90909cd17c89abececca152
Jun Matsushita <jun@iilab.org>
2018-01-14 14:56:50 +0100

    Cleanup

--- src/index.js
+++ src/index.js
@@ -71,8 +71,6 @@ function plugin(options) {
       .mapValues(transform)
       .value();
 
-    console.log('res', res);
-
     Object.keys(files).forEach(key => {
       // debug('<< File keys: ', Object.keys(files[key]));
       // debug('<< Res keys: ', Object.keys(res[key]));
@@ -84,87 +82,5 @@ function plugin(options) {
     });
 
     return;
-    // const process_old = (file, key, cb) => {
-    //
-    //   const resolvePermalinks = (url, source, placeholder) => {
-    //     debug('>>> resolvePermalinks      :', url);
-    //     // const targetKey = path.join(path.dirname(key), url);
-    //     // const matches = Object.keys(files).filter(key => key.startsWith(targetKey));
-    //     return { content: placeholder };
-    //   };
-    //
-    //   // # Invariants:
-    //   //
-    //   // ## patterns commute with juxtaposition
-    //   //
-    //   // :[](browse/activity)
-    //   // :[](browse/context)
-    //   // :[](browse/framework)
-    //   //
-    //   // process to the same as
-    //   // :[](browse)
-    //   //
-    //   // where browse is a folder containing activity.md / context.md / framework.md
-    //
-    //
-    //   const resolveFolders = (url, source, placeholder) => {
-    //     debug('>>> resolveFolders        :', url);
-    //     const targetKey = path.join(path.dirname(key), url);
-    //     const matches = Object.keys(files).filter(key => key.startsWith(targetKey));
-    //     debug('>>> Using targetKey       :', targetKey);
-    //     if (matches.length == 0) {
-    //       debug('>>> No target files found');
-    //       return null;
-    //     }
-    //     debug('>>> Found target files    :', matches.join(' '));
-    //
-    //     const content = matches
-    //       .map(value => path.join(url, value.replace(targetKey.split(url + '/').slice(-1).join('/'), '')))
-    //       .map(value => placeholder.replace(url, value))
-    //       .join('\n');
-    //
-    //     debug('>>> Content               :', content);
-    //
-    //     return {
-    //       content
-    //     };
-    //   };
-    //
-    //   // Return link if the target cannot be resolved.
-    //   const resolvers = [
-    //     /*resolvePermalinks, */ resolveFolders,
-    //     (url, source, placeholder) => ({ content: placeholder })
-    //   ];
-    //
-    //
-    //
-    //   hercule.transcludeString(file.contents, { resolvers }, (err, result) => {
-    //     // if (err && err.code === 'ENOENT') {
-    //     //   debug("Couldn't find the following file and skipped it. " + err.path);
-    //     //   return cb();
-    //     // }
-    //     if (err) {
-    //       console.error(err);
-    //       return cb(err);
-    //     }
-    //     // debug('<< Result: ', result);
-    //     debug('<< Finished processing file: ', key);
-    //     return cb(null, { ...file, contents: new Buffer(result) });
-    //   });
-    // };
-    //
-    // async.mapValuesSeries(files, transform, (err, res) => {
-    //   if (err) throw err;
-    //   Object.keys(files).forEach(key => {
-    //     // debug('<< File keys: ', Object.keys(files[key]));
-    //     // debug('<< Res keys: ', Object.keys(res[key]));
-    //     if (res[key]) {
-    //       files[key] = res[key];
-    //     } else {
-    //       delete files[key];
-    //     }
-    //   });
-    //   done();
-    // });
   };
 }
